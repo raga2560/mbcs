a6d5d1066852ae3129c2e1a7e853bdedb6dc3e53
Ramesh <raga2560@gmail.com>
2018-04-24 16:09:01 +0530

    prog

---
+++ linker.js
@@ -0,0 +1,32 @@
+ 
+sinkaddr = require("sinkaddress");
+const assert = require('assert');
+
+const bluebird = require("bluebird");
+
+//var sinkaddrp = bluebird.promisifyAll((sinkaddr), {suffix: "Promise"});
+
+
+
+function linker () {
+
+
+ this.getSAfromPKandRedeem = function(Pk1, Pk2, Pk3, network, callback) {
+var sa1=sinkaddr.sinkaddresslib.getSAfromPK(Pk1,Pk2, Pk3, network);
+    var sa = sinkaddr.sinkaddresslib.getSAfromPK(Pk1, Pk2, Pk3, network);
+    var rd = sinkaddr.sinkaddresslib.getRDScriptfromPK(Pk1, Pk2, Pk3, network);
+
+     var data =
+	{
+	linkaddress: sa,
+	redeemscript : rd
+	};
+	callback (null, data);
+ }
+
+
+
+}
+
+module.exports.linker = linker;
+
--- routes/database.js
+++ routes/database.js
@@ -1,9 +1,11 @@
+var linkerDB = require('../linker').linker;
 	
 
 
 var databaseDBB = require('../databasestore').DatabaseStore;
 module.exports = function(io, db) {
  
+	var linker = new linkerDB();
     var database = io.of('/database');
 	var databaseDB = new databaseDBB(db);
  
@@ -22,16 +24,43 @@ module.exports = function(io, db) {
               });
         });
         socket.on('issuelink', function(sentdata) {
-			console.log('issuelink');
-	var data = {
-	publickey: 'abcd',
-	redeemscript: 'aybcd',
-	linkaddress: 'ab8cd'
+			console.log('issuelink:'+ JSON.stringify(sentdata));
+	var data1 = {
+	publickey: sentdata.data.publickey,
+	redeemscript: '',
+	linkaddress: ''
 	};
-            databaseDB.saveTodo(data, function(err, data) {
+        var bankwif = 'cRgnQe1TQngWfX53nSbjuiBzpY1kB5aDVZBFyegf5jAvQGZPojA1';
+        var controllerwif = 'cRgnQe1TQngWfX53nSbjTGp3BwvrzawVYLqyR6qAr3zU16T3b2FB';
+        var bankkeyPair = bitcoin.ECPair.fromWIF(
+     		bankwif,
+     		bitcoin.networks.testnet);
+
+        var controllerkeyPair = bitcoin.ECPair.fromWIF(
+     		controllerwif,
+     		bitcoin.networks.testnet);
+
+	var bankpubkey = bankkeyPair.getPublicKeyBuffer();
+
+	var controllerpubkey = controllerkeyPair.getPublicKeyBuffer();
+        var network = sentdata.network;
+
+	//console.log(sentdata.data.publickey);
+        var sentpubkeybuffer = Buffer.from(sentdata.data.publickey);
+        linker.getSAfromPKandRedeem(bankpubkey, 
+			sentpubkeybuffer,
+			controllerpubkey,
+		        network, function (err, data) { 
+	//console.log(data);
+           data1.linkaddress = data.linkaddress;
+           data1.redeemscript = data.redeemscript.toString();
+
+            databaseDB.saveTodo(data1, function(err, data) {
                 if (err) throw err; // You can emit the error to a socket	
 	io.of('/database').emit('issuedlink', data);
             });
+	  });
+
         });
  
         socket.on('saveTodo', function(todo) {
